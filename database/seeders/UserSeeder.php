641ba704f900fabb2af4b99768e191e7b7853384
gil cordeiro <gilwindbill@gmail.com>
2021-05-09 16:27:45 -0300

    projeto funcionando

---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+
+        DB::table('users')->delete();
+
+        $users = array(
+            ['name' => 'Anna Beatriz', 'email' => 'annabeatriz@gmail.com', 'password' => Hash::make('123')],
+            ['name' => 'Robson Nunes', 'email' => 'robsonnuens@gmail.com', 'password' => Hash::make('123')],
+        );
+
+        // Loop through each user above and create the record for them in the database
+        foreach ($users as $user) {
+            User::create($user);
+        }
+    }
+}
